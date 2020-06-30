45f6704a8f6cf7ef24bcc71796404a6e758561a9
thually <alejosalazarmejia.2000@gmail.com>
2020-06-30 16:19:12 -0500

    finished 3rd plot

---
+++ plot3.R
@@ -0,0 +1,19 @@
+library(lubridate)
+library(dplyr)
+
+fulldata <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
+data <- fulldata %>% filter(Date == "1/2/2007" | Date == "2/2/2007")
+data <- data %>% mutate(Date_time = paste(Date, Time))
+data <- data %>% mutate(Date_time = dmy_hms(Date_time))
+
+with(data, {plot(Date_time, Sub_metering_1, type = "l",
+                ylab = "Energy sub metering",
+                xlab = "")
+    points(Date_time, Sub_metering_2, type = "l", col = "red")
+    points(Date_time, Sub_metering_3, type = "l", col = "blue")
+    legend("topright", lty = "solid", col = c("black", "red", "blue"),
+           legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+    })
+
+dev.copy(png, filename = "plot3.png")
+dev.off()
