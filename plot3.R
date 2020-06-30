a4086634b1a1a115e700fd62ffa537ee966ba5bf
thually <alejosalazarmejia.2000@gmail.com>
2020-06-30 16:54:26 -0500

    This is the one

--- plot3.R
+++ plot3.R
@@ -6,15 +6,15 @@ data <- fulldata %>% filter(Date == "1/2/2007" | Date == "2/2/2007")
 data <- data %>% mutate(Date_time = paste(Date, Time))
 data <- data %>% mutate(Date_time = dmy_hms(Date_time))
 
+png("plot3.png")
 with(data, {plot(Date_time, Sub_metering_1, type = "l",
                 ylab = "Energy sub metering",
                 xlab = "")
     points(Date_time, Sub_metering_2, type = "l", col = "red")
     points(Date_time, Sub_metering_3, type = "l", col = "blue")
     legend("topright", lty = "solid", col = c("black", "red", "blue"),
-           legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-           cex = 0.75, text.width = 40000)
+           legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
     })
 
-dev.copy(png, filename = "plot3.png")
+
 dev.off()
