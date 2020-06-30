695686d0a81207d386e4abb46ae228582c26dee8
thually <alejosalazarmejia.2000@gmail.com>
2020-06-30 16:00:53 -0500

    finished 2nd plot

---
+++ plot2.R
@@ -0,0 +1,14 @@
+library(lubridate)
+library(dplyr)
+
+fulldata <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
+data <- fulldata %>% filter(Date == "1/2/2007" | Date == "2/2/2007")
+data <- data %>% mutate(Date_time = paste(Date, Time))
+data <- data %>% mutate(Date_time = dmy_hms(Date_time))
+
+with(data, plot(Date_time, Global_active_power, type = "l",
+                ylab = "Global Active Power (kilowatts)",
+                xlab = ""))
+
+dev.copy(png, filename = "plot2.png")
+dev.off()
